14df760b47022cc7dd59b9d393d7ef7aee166819
KartiKChaudhary07 <kartik9983chaudhary@gmail.com>
2026-03-10 12:25:38 +0530

    feature/UC10-Generic-Quantity-Class-with-Unit-Interface-For-Multi-Category-Support

--- quantitymeasurementapp/src/main/java/QuantityMeasurementApp.java
+++ quantitymeasurementapp/src/main/java/QuantityMeasurementApp.java
@@ -1,34 +1,21 @@
 public class QuantityMeasurementApp {
+
     public static void main(String[] args) {
-        Length oneFoot = new Length(1.0, Length.LengthUnit.FEET);
-        Length twelveInch = new Length(12.0, Length.LengthUnit.INCH);
-        Length oneYard = new Length(1.0, Length.LengthUnit.YARDS);
-        Length cm = new Length(30.48, Length.LengthUnit.CENTIMETER); // ≈ 1 foot
 
-        System.out.println("1 ft == 12 in : " + oneFoot.equals(twelveInch));
-        System.out.println("1 yard == 3 ft : " + oneYard.equals(new Length(3.0, Length.LengthUnit.FEET)));
-        System.out.println("30.48 cm == 1 ft : " + cm.equals(oneFoot));
+        Quantity<LengthUnit> l1 = new Quantity<>(1, LengthUnit.FEET);
+        Quantity<LengthUnit> l2 = new Quantity<>(12, LengthUnit.INCH);
 
-        Length feet = new Length(1.0, Length.LengthUnit.FEET);
-        Length inch = new Length(2.0, Length.LengthUnit.INCH);
+        System.out.println("Length equal? " + l1.equals(l2));
 
-        Length result1 = feet.add(inch);
-        System.out.println("1 ft + 2 in = " + result1);
+        Quantity<WeightUnit> w1 = new Quantity<>(1, WeightUnit.KILOGRAM);
+        Quantity<WeightUnit> w2 = new Quantity<>(1000, WeightUnit.GRAM);
 
-     
-        Length result2 = inch.add(feet);
-        System.out.println("2 in + 1 ft = " + result2);
+        System.out.println("Weight equal? " + w1.equals(w2));
 
+        System.out.println("Convert 1 foot to inches: " + l1.convertTo(LengthUnit.INCH));
 
-        Length yard = new Length(1.0, Length.LengthUnit.YARDS);
-        Length feetTwo = new Length(2.0, Length.LengthUnit.FEET);
+        System.out.println("Add lengths: " + l1.add(l2));
 
-        Length result3 = yard.add(feetTwo);
-        System.out.println("1 yard + 2 ft = " + result3);
-        Length zero = new Length(0.0, Length.LengthUnit.FEET);
-        Length negative = new Length(-2.0, Length.LengthUnit.FEET);
-
-        System.out.println("1 ft + 0 ft = " + feet.add(zero));
-        System.out.println("5 ft + (-2 ft) = " + new Length(5.0, Length.LengthUnit.FEET).add(negative));
+        System.out.println("Add weights in grams: " + w1.add(w2, WeightUnit.GRAM));
     }
 }
